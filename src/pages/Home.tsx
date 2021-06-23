4e15f59c7df98b0fbb9a648ec430929c05bbf551
Eliezer <eliezerlincon537@hotmail.com>
2021-06-22 22:08:44 -0300

    Sign in with Google done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,7 @@
+import { useHistory } from 'react-router-dom';
+
+import { useAuth } from '../hooks/useAuth';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
@@ -7,6 +11,17 @@ import { Button } from '../components/Button';
 import '../styles/auth.scss';
 
 export function Home() {
+	const history = useHistory();
+	const { user, SignInWithGoogle } = useAuth();
+
+	async function handleCreateRoom() {
+		if (!user) {
+			await SignInWithGoogle();
+		}
+
+		history.push('/rooms/new');
+	}
+
 	return (
 		<div id='page-auth'>
 			<aside>
@@ -21,7 +36,7 @@ export function Home() {
 			<main>
 				<div className='main-content'>
 					<img src={logoImg} alt='logo da aplicação' />
-					<button className='create-room'>
+					<button className='create-room' onClick={handleCreateRoom}>
 						<img src={googleIconImg} alt='Logo do Google' />
 						Crie sua sala com o Google
 					</button>

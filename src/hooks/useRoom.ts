97594bfe77a654be5f053190b42169c51c2ba257
Eliezer <eliezerlincon537@hotmail.com>
2021-06-24 02:13:58 -0300

    Resource to like, done

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { database } from '../services/firebase';
+import { useAuth } from './useAuth';
 
 // prettier-ignore
 type FirebaseQuestions = Record<string, {
@@ -10,6 +11,9 @@ type FirebaseQuestions = Record<string, {
 	content: string;
 	isHighlighted: boolean;
 	isAnswered: boolean;
+	likes: Record<string, {
+		authorId: string;
+	}>
 }>;
 
 type QuestionType = {
@@ -21,9 +25,12 @@ type QuestionType = {
 	content: string;
 	isHighlighted: boolean;
 	isAnswered: boolean;
+	likeCount: number;
+	likeId: string | undefined;
 };
 
 export function useRoom(roomId: string) {
+	const { user } = useAuth();
 	const [questions, setQuestions] = useState<QuestionType[]>([]);
 	const [title, setTitle] = useState('');
 
@@ -36,13 +43,15 @@ export function useRoom(roomId: string) {
 			const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
 
 			// prettier-ignore
-			const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+			const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {	
 					return {
 						id: key,
 						content: value.content,
 						author: value.author,
 						isHighlighted: value.isHighlighted,
 						isAnswered: value.isAnswered,
+						likeCount: Object.values(value.likes ?? {}).length,
+						likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0],
 					};
 				}
 			);
@@ -50,7 +59,11 @@ export function useRoom(roomId: string) {
 			setTitle(databaseRoom.title);
 			setQuestions(parsedQuestions);
 		});
-	}, [roomId]);
+
+		return () => {
+			roomRef.off('value');
+		};
+	}, [roomId, user?.id]);
 
 	return { questions, title };
 }
